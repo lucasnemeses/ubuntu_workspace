73d9d472ea4cc09b7c60e24b15498904f0ef5370
Lucas Meneses <52137774+lucasnemeses@users.noreply.github.com>
2022-03-28 10:54:40 -0300

    Update oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -18,7 +18,7 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.o
 sed -i 's+plugins=(git)+plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)+g' ~/.zshrc &&
 
 ## Configurando Docker ##
-sed -i 's+export ZSH=$HOME/.oh-my-zsh+export ZSH=$HOME/.oh-my-zsh\nexport WWWUSER=${WWWUSER:-$UID}\nexport WWWGROUP=${WWWGROUP:-$(id -g)}+g' ~/.zshrc &&
+sed -i 's+export ZSH="$HOME/.oh-my-zsh"+export ZSH="$HOME/.oh-my-zsh"\nexport WWWUSER=${WWWUSER:-$UID}\nexport WWWGROUP=${WWWGROUP:-$(id -g)}+g' ~/.zshrc 
 
 ## Adicionando Alias ##
 sed -i 's+# alias ohmyzsh="mate ~/.oh-my-zsh"+alias app="cd ~/app/"\nalias docker-up="docker-compose up -d"\nalias docker-down="docker-compose down"\nalias docker-bash="docker-compose exec app bash"\nalias docker-remove="docker rm -f $(docker ps --format '{{.ID}}')"+g' ~/.zshrc &&
